33f6845e2aa96c5b671498dadbd478366b3f0bc0
gonxi022 <charitosanchez.66a@gmail.com>
2025-07-25 17:39:55 -0300

    Crear lua2k.lua

---
+++ lua2k.lua
@@ -0,0 +1,30 @@
+--h2k Script
+local player = game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui")
+gui.Parent = player:WaitForChild("PlayerGui")
+gui.ResetOnSpawn = false
+
+local speed = Instance.new("TextButton")
+speed.Name = "Speed"
+speed.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+speed.Size = UDim2.new(0, 150, 0, 50)
+speed.Position = UDim2.new(0, 120, 0, 40)
+speed.Text = "Speed off"
+speed.TextColor3 = Color3.fromRGB(255, 255, 255)
+speed.Parent = gui
+
+local speedActive = false
+
+speed.MouseButton1Click:Connect(function()
+  speedActive = not speedActive
+  if speedActive then
+    player.Character.Humanoid.WalkSpeed = 60
+    speed.Text = "Speed On"
+    speed.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+    else
+      player.Character.Humanoid.WalkSpeed = 16
+      speed.Text = "Speed Off"
+      speed.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+  end
+end)
+  
